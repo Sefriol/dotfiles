a269a07a498d75747543192629b1a45099044183
Joakim Järvinen <joakim.jarvinen@brightlyworks.com>
2024-07-15 16:49:00 +0300

    feat: add alacritty config

--- .install/install_osx.zsh
+++ .install/install_osx.zsh
@@ -13,20 +13,7 @@ brew install antidote
 
 
 echo "Installing font(s): MesloLGS NF for Powerlevel10k"
-cd /Library/Fonts && {
-    curl -O 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf';
-    mv MesloLGS%20NF%20Regular.ttf "MesloLGS NF Regular.ttf";
-    curl -O 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf';
-    mv MesloLGS%20NF%20Bold.ttf "MesloLGS NF Bold.ttf";
-    curl -O 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf';
-    mv MesloLGS%20NF%20Italic.ttf "MesloLGS NF Italic.ttf";
-    curl -O 'https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf';
-    mv MesloLGS%20NF%20Bold%20Italic.ttf "MesloLGS NF Italic.ttf";
-    cd -;
-}
-echo "REMINDER: Fonts need to be set for each Terminal / IDE manually"
-echo "DOCS: https://github.com/romkatv/powerlevel10k/blob/master/font.md"
-
+brew install font-meslo-lg-nerd-font
 # Copy configs. i.e. copy all files in this repository except this install directory
 echo "Creating Symbolic links to $HOME"
 
